27a82ff377ee002e600fb211fbfed576cda9c540
Rubens <rubens.sautter@gmail.com>
2022-08-01 15:48:44 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='zetaSpace',
+    version='0.1',
+    url='https://github.com/rsautter/Zeta-Space',
+    author='Rubens Andreas Sautter',
+    author_email='rubens.sautter@gmail.com',
+    keywords='Multifractal PSD Time-series',
+    description=u'Zeta Space Package (spectral entropy x delta alpha)',
+     packages=find_packages()
+)
