235776c610e5b081f4c232bcd02ee89793e00259
Rubens <rubens.sautter@gmail.com>
2022-08-01 16:47:18 -0300

    CaptionError

--- zeta/zetaSpace.py
+++ zeta/zetaSpace.py
@@ -4,4 +4,4 @@ import numpy as np
 
 def zetaSpace(data,qs=np.array([5,6,7,8,9])):
 	a, fa, _, _, _ = autoMDFDA(data,qs=qs)
-	return {"spectral_entropy":SpectralEntropy(data), "delta_alpha":singularitySpectrumMetrics(a,fa)["delta_alpha"]}
+	return {"spectral_entropy":spectralEntropy(data), "delta_alpha":singularitySpectrumMetrics(a,fa)["delta_alpha"]}
