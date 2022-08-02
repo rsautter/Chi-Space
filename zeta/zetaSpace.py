6ff9de104c2b13da593412dd72c9fc8c38d6022f
Rubens <rubens.sautter@gmail.com>
2022-08-02 06:49:37 -0300

    Revert "Testing some weird colab problems"
    This reverts commit 0e980b9bb7c96b7d53893a594c92384fcf6fb607.

--- zeta/zetaSpace.py
+++ zeta/zetaSpace.py
@@ -3,7 +3,5 @@ from .singularitySpectrum import *
 import numpy as np
 
 def zetaSpace(data,qs=np.array([5,6,7,8,9])):
-	a, fa, als, fals, _ = autoMDFDA(data,qs=qs)
-	print(als)
-	print(fals)
+	a, fa, _, _, _ = autoMDFDA(data,qs=qs)
 	return {"spectral_entropy":spectralEntropy(data), "delta_alpha":singularitySpectrumMetrics(a,fa)["delta_alpha"]}
