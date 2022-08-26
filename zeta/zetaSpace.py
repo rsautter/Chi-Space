fd3bd29c380e52156374155df9b129133c2987eb
Rubens <rubens.sautter@gmail.com>
2022-08-26 14:38:11 -0300

    Updated to Q-Q Plot
    Instead of using thespeactral entropy, this version uses Q-Q Plot distance metric

--- zeta/zetaSpace.py
+++ zeta/zetaSpace.py
@@ -1,8 +1,9 @@
 from .spectralEntropy import *
 from .singularitySpectrum import *
+from .qqMetric import *
 import numpy as np
 
-def zetaSpace(data,qs=np.arange(5,15,2), scThresh=1e-2, nqs = 10, nsamples=40, nscales=20,magnify=7,**kwargs):
+def zetaSpace(data,qs=np.arange(5,15,2), scThresh=1e-2, nqs = 10, nsamples=40, nscales=20,magnify=7):
 	'''
 	========================================================================
 	
@@ -14,14 +15,13 @@ def zetaSpace(data,qs=np.arange(5,15,2), scThresh=1e-2, nqs = 10, nsamples=40, n
 	qs - Hurst exponents extremes
 	scThresh - scale threshold of autoMFDFA
 	nqs - number of hurst exponents
-	**kwargs - PSD parameters
 	========================================================================
 	Output:
-	Dictionary with keywords 'ISE' and 'LDA', where:
-	ISE - Inverse Spectral Entropy
+	Dictionary with keywords 'GQQ' and 'LDA', where:
+	GQQ - Gaussian Q-Q Plot distance
 	LDA - Logistic Delta Alpha
 	========================================================================
 	Wrote by: Rubens A. Sautter (08/2022)
 	'''
 	_,_, lda = autoMFDFA(data,qs=qs,scThresh=scThresh,nqs=nqs,nsamples=nsamples, nscales=nscales,magnify=magnify)
-	return {"ISE":1-spectralEntropy(data,*kwargs), "LDA":lda}
+	return {"GQQ":qqGaussianDistance(data), "LDA":lda}
